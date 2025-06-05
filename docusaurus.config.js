5b3f09c36d40ce3c1d902b16ccc0e227eaef4bd6
Yonas <yonas.mersha14@gmail.com>
2025-06-05 17:38:50 +0300

    docusaurus config base url 1

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -46,7 +46,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/YonSci/eca-census-handbook',
         },
         theme: {
           customCss: './src/css/custom.css',
@@ -148,7 +148,7 @@ const config = {
             items: [
               {
                 label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
+                href: 'https://github.com/YonSci/eca-census-handbook',
               },
             ],
           },
