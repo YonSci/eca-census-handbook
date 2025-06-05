6f9b7fded8c25696795df5a84a1a6816faf3fd2c
Yonas <yonas.mersha14@gmail.com>
2025-06-05 17:16:17 +0300

    first-commit-census-handbook

---
+++ docusaurus.config.js
@@ -0,0 +1,165 @@
+// @ts-check
+// `@type` JSDoc annotations allow editor autocompletion and type checking
+// (when paired with `@ts-check`).
+// There are various equivalent ways to declare your Docusaurus config.
+// See: https://docusaurus.io/docs/api/docusaurus-config
+
+import {themes as prismThemes} from 'prism-react-renderer';
+
+// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'E-Census Handbook',
+  tagline: 'Documentation for E-Census Handbook',
+  favicon: 'img/uneca1.ico',
+
+  // Set the production url of your site here
+  url: 'https://your-docusaurus-site.example.com',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'facebook', // Usually your GitHub org/user name.
+  projectName: 'docusaurus', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: './sidebars.js',
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      // Replace with your project's social card
+      image: 'img/docusaurus-social-card.jpg',
+      navbar: {
+        title: 'E-Census Handbook',
+        logo: {
+          alt: 'My Site Logo',
+          src: 'img/uneca1.png',
+        },
+        items: [
+          {
+            type: 'docSidebar',
+            sidebarId: 'experienceslessons2020Sidebar',
+            position: 'left',
+            label: 'Experiences & lessons from 2020',
+          },
+          {
+            type: 'docSidebar',
+            sidebarId: 'casestudiesSidebar',
+            position: 'left',
+            label: 'Case Studies',
+          },
+          {
+            type: 'docSidebar',
+            sidebarId: 'recommendationsSidebar',
+            position: 'left',
+            label: 'Recommendations',
+          },
+          {
+            type: 'docSidebar',
+            sidebarId: 'resourcesSidebar',
+            position: 'left',
+            label: 'Resources',
+          },
+          {
+            href: 'https://github.com/facebook/docusaurus',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Handbook',
+            items: [
+              {
+                label: 'Experiences & Lessons',
+                to: '/docs/experiences-lessons-2020/ch1',
+              },
+
+              {
+                label: 'Case Studies',
+                to: '/docs/case-studies/ch1',
+              },
+
+              {
+                label: 'Recommendations',
+                to: '/docs/recommendations/ch1',
+              },
+
+              {
+                label: 'Resources',
+                to: '/docs/resources/ch1',
+              },
+
+            ],
+          },
+          // {
+          //   title: 'Community',
+          //   items: [
+          //     {
+          //       label: 'Stack Overflow',
+          //       href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+          //     },
+          //     {
+          //       label: 'Discord',
+          //       href: 'https://discordapp.com/invite/docusaurus',
+          //     },
+          //     {
+          //       label: 'X',
+          //       href: 'https://x.com/docusaurus',
+          //     },
+          //   ],
+          // },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'GitHub',
+                href: 'https://github.com/facebook/docusaurus',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} UNECA, Inc. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: prismThemes.github,
+        darkTheme: prismThemes.dracula,
+      },
+    }),
+};
+
+export default config;
---
+++ src/components/HomepageFeatures/index.js
@@ -0,0 +1,62 @@
+import clsx from 'clsx';
+import Heading from '@theme/Heading';
+import styles from './styles.module.css';
+
+const FeatureList = [
+  {
+    title: 'Population and Housing Censuses (PHC)',
+    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    description: (
+      <>
+       PHC provide countries with fundamental demographic and socio-economic information. This information is key for planning, programming, and monitoring international, regional, and national commitments.
+      </>
+    ),
+  },
+  {
+    title: 'Census E-Handbook',
+    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    description: (
+      <>
+        Census e-handbook has been developed to serve as a reference document for African countries in undertaking digital censuses.
+      </>
+    ),
+  },
+  {
+    title: 'Lessons Learned and Good Practices',
+    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    description: (
+      <>
+        The e-handbook provides a repository of experiences, key lessons learned and good practices in planning and implementation of digital censuses in Africa during the 2020 Round of Population and Housing Censuses.
+      </>
+    ),
+  },
+  
+];
+
+function Feature({Svg, title, description}) {
+  return (
+    <div className={clsx('col col--4')}>
+      <div className="text--center">
+        <Svg className={styles.featureSvg} role="img" />
+      </div>
+      <div className="text--center padding-horiz--md">
+        <Heading as="h2">{title}</Heading>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+}
+
+export default function HomepageFeatures() {
+  return (
+    <section className={styles.features}>
+      <div className="container">
+        <div className="row">
+          {FeatureList.map((props, idx) => (
+            <Feature key={idx} {...props} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
