17a8b4e5253c74b68f96a03c28e9386bf985fa03
Yonas <yonas.mersha14@gmail.com>
2025-06-05 18:10:10 +0300

    docusaurus config constant config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,15 +15,16 @@ const config = {
   favicon: 'img/uneca1.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://github.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/eca-census-handbook',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'YonSci', // Usually your GitHub org/user name.
+  projectName: 'eca-census-handbook', // Usually your repo name.
+  deploymentBranch: 'gh-pages', // The branch that GitHub pages will deploy from.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
