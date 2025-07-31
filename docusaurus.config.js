6d27b07c60ed58468a69b992eeddf895d32fb381
Yonas <yonas.mersha14@gmail.com>
2025-07-31 18:06:50 +0300

    Update Docusaurus configuration to set base URL for Netlify deployment, commenting out the previous GitHub Pages base URL for clarity.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,7 +12,9 @@ const config = {
   url: 'https://yonsci.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/eca-census-handbook/',
+  // baseUrl: '/eca-census-handbook/',   // GitHub pages
+  baseUrl: '/',  // Netlify 
+
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
